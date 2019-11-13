6fb9160687ab00f86b2caa64667d0486f0e0004a
JSPlike <moter2588@gmail.com>
2019-11-12 18:47:49 -0800

    routes/index users

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({
-    message: "this is basic page"
-  });
+router.get('/api/consent', function(req, res, next) {
+  var i_d = "user";
+  var count = 1;
+  var random_id = i_d + count;
+
+  console.log(random_id);
 });
 
 module.exports = router;
