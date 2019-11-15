1850ee8e33c26d50737f79bf0a01197cead83a12
JSPlike <moter2588@gmail.com>
2019-11-15 02:44:23 -0800

    /consent

--- routes/index.js
+++ routes/index.js
@@ -17,17 +17,18 @@ router.get('/consent', function(req, res, next) {
   for(var i = 0; i < 4; i++) {
     //Math.random => 0~1 random float num
     var n = Math.floor(Math.random() * random_base_char.length);
-    temp_1 += random_base_char[n];
+    temp_1 += random_base_char.substring(n, n + 1);
   }
 
   // num length = 5
   for(var j = 0; j < 5; j++){
     var m = Math.floor(Math.random() * random_base_num.length);
-    temp_2 += random_base_num[m];
+    temp_2 += random_base_num.substring(m, m + 1);
   }
 
   var resultCode = temp_1.concat(temp_2);
-  return res.json({code: resultCode});
+
+  res.json(resultCode);
 
   // var realUser = new Usercode();
   // realUser.UserCode = resultCode;
@@ -48,8 +49,6 @@ router.get('/consent', function(req, res, next) {
   //     console.log('Success success User Count!!');
   //   });
   // });
-
-
 });
 
 router.get('/reject', function(req, res, next){
